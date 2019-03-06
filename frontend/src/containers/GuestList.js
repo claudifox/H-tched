ec9309664d7222d742dfc210be4ff4ee79eb1508
Claudia Fox <claudiafox95@icloud.com>
2019-03-06 10:34:08 +0000

    Added basic Guest List Container

---
+++ frontend/src/containers/GuestList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import GuestTable from '../components/GuestTable'
+// import GuestListForm from '../components/GuestListForm'
+
+export default class GuestList extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <GuestTable />
+      </div>
+    )
+  }
+}
